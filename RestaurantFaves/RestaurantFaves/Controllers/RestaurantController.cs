95feabd840e29a0f01d91ad09a0f4a1449dec167
Ramsey Louder <RamseyLouder@gmail.com>
2024-06-23 22:40:58 -0400

    restaurant faves

---
+++ RestaurantFaves/RestaurantFaves/Controllers/RestaurantController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantFaves.Models;
+
+namespace RestaurantFaves.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+        RestaurantFavesContext DbContext = new RestaurantFavesContext();
+        //api/restaurant
+
+        [HttpGet()]
+
+        public IActionResult GetOrders(string? r = null, bool? orderagain = null)
+        {
+            List<RestaurantRating> result = DbContext.RestaurantRatings.ToList();
+            if (r != null)
+            {
+                result = result.Where(b => b.Restaurant.ToLower().Contains(r.ToLower())).ToList();
+            }
+            if (orderagain != null)
+            {
+                result = result.Where(b => b.OrderAgain == orderagain).ToList();
+            }
+            return Ok(result);
+        }
+        //get by id
+        //api/restaurantratings/2
+        [HttpGet("{id})")]
+
+        public IActionResult GetOrdersById(int id)
+        {
+            RestaurantRating result = DbContext.RestaurantRatings.Find(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+        //add restaurant rating
+        //api/restaurant/3
+        [HttpPost()]
+        public IActionResult AddOrder([FromBody] RestaurantRating newOrder)
+        {
+            newOrder.Id = 0;
+            DbContext.RestaurantRatings.Add(newOrder);
+            DbContext.SaveChanges();
+            return Created($"/RestaurantRating/Api/{newOrder.Id}", newOrder);
+        }
+        //update order
+        //api/restaurantratings/3
+        [HttpPut("{id}")]
+        public IActionResult UpdateOrder(int id, [FromBody] RestaurantRating updateOrder)
+        {
+             if (updateOrder.Id != id) { return BadRequest(); }
+             if(!DbContext.RestaurantRatings.Any(b=> b.Id == id)) { return NotFound(); }
+            DbContext.RestaurantRatings.Update(updateOrder);
+            DbContext.SaveChanges() ;
+            return NoContent(); 
+        }
+        //delete book
+        //api/restaurantrating/4
+        [HttpDelete("{id}")]
+        public IActionResult DeleteOrder(int id)
+        {
+            RestaurantRating result = DbContext.RestaurantRatings.Find(id);
+            if (result == null) { return NotFound();}
+            DbContext.RestaurantRatings.Remove(result);
+            DbContext.SaveChanges();
+            return NoContent();
+        }
+
+
+
+
+
+    }
+}
---
+++ RestaurantFaves/RestaurantFaves/Models/RestaurantFavesContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantFaves.Models;
+
+public partial class RestaurantFavesContext : DbContext
+{
+    public RestaurantFavesContext()
+    {
+    }
+
+    public RestaurantFavesContext(DbContextOptions<RestaurantFavesContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<RestaurantRating> RestaurantRatings { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=Restaurant_Faves; Integrated Security=SSPI;Encrypt=false;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<RestaurantRating>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Restaura__3214EC078415DF79");
+
+            entity.Property(e => e.Description).HasMaxLength(255);
+            entity.Property(e => e.Restaurant).HasMaxLength(255);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
